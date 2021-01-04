84b38a21537db9cdc8f73add769fa9f6c586dd12
Maurice Frank <maurice.frank@posteo.de>
2021-01-04 22:29:22 +0100

    renamed package

--- server.py
+++ server.py
@@ -6,20 +6,20 @@ from sanic import response
 from sanic_jinja2 import SanicJinja2
 from sanic_httpauth import HTTPBasicAuth
 
-import cloud
+import b2browser
 import config
 
-app = Sanic("cloud")
+app = Sanic("b2browser")
 app.static("/static", "./static")
 auth = HTTPBasicAuth()
-jinja = SanicJinja2(app, pkg_name="cloud")
+jinja = SanicJinja2(app, pkg_name="b2browser")
 
-cloud.B2.setup(config.application_key_id, config.application_key, config.bucket_name)
+b2browser.B2.setup(config.application_key_id, config.application_key, config.bucket_name)
 
 
 @auth.verify_password
 def verify_password(username, password):
-    return username == config.username and cloud.utils.hash_password(config.app_salt, password) == config.password
+    return username == config.username and b2browser.utils.hash_password(config.app_salt, password) == config.password
 
 
 @app.listener("after_server_start")
@@ -28,28 +28,28 @@ def create_task_queue(app, loop):
     app.thread_executor = ThreadPoolExecutor(config.thread_pool_size)
 
     for i in range(config.thread_pool_size):
-        app.add_task(cloud.preview.worker(f"thumbnail_{i}", app.thumbnail_queue, app.thread_executor))
+        app.add_task(b2browser.preview.worker(f"thumbnail_{i}", app.thumbnail_queue, app.thread_executor))
 
 
 @jinja.template("file.html")
 async def single(request, path):
-    file = cloud.LS_CACHE[path]
-    src = cloud.B2.cache(file.id, file.path)
+    file = b2browser.LS_CACHE[path]
+    src = b2browser.B2.cache(file.id, file.path)
     return {
         "src": src,
         "file": file,
-        "breadcrumb": cloud.utils.make_crumbs(path),
+        "breadcrumb": b2browser.utils.make_crumbs(path),
     }
 
 
 @jinja.template("folder.html")
 async def folder(request, path):
-    folders, files = cloud.B2.ls(path)
-    cloud.preview.queue(files, request.app.thumbnail_queue)
+    folders, files = b2browser.B2.ls(path)
+    b2browser.preview.queue(files, request.app.thumbnail_queue)
     return {
         "folders": folders,
         "files": files,
-        "breadcrumb": cloud.utils.make_crumbs(path),
+        "breadcrumb": b2browser.utils.make_crumbs(path),
     }
 
 
@@ -62,7 +62,7 @@ async def root(request):
 @app.route("/<path:path>")
 @auth.login_required
 async def non_root(request, path: str):
-    if cloud.ls_cache.is_file(path):
+    if b2browser.ls_cache.is_file(path):
         return await single(request, path)
     else:
         return await folder(request, path)
